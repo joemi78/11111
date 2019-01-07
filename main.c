34bc1c483e3bdf095efcdb17a30bed4999cb4d38
Unknown <unknown@example.com>
2019-01-07 15:13:53 +0800

    printf num

--- main.c
+++ main.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+void peintf_num(int num);
 int main()
 {
     printf("Hello world!\n");
     printf("54564564");
+    peintf_num(5);
     return 0;
+}
+
+void peintf_num(int num)
+{
+    int i;
+    for(i=num;i<10;i=i+1)
+    {
+        printf(" %d \n",i);
+    }
 }
